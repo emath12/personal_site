ac98d4a4be00b55e475ffb7422b60a9d21819067
Ethan M <ethan.mathieu@yale.edu>
2024-03-09 16:22:17 -0500

    diffs

--- src/components/Page.js
+++ src/components/Page.js
@@ -82,7 +82,7 @@ function AboutMe() {
             {isPlaying && <a href="https://triangle-strategy.fandom.com/wiki/Benedict_Pascal"><p className='text-white ml-3 bg-gray-700 p-2 rounded-lg opacity-90 drop-shadow-lg'>Benedict's Path</p></a>}
         </div>
 
-        <div className="m-8 lg:m-20 md:m-20 sm:m-20 w-full font-medium">
+        <div className="m-6 lg:m-20 md:m-20 sm:m-20 w-full font-medium">
             <div className="lg:text-4xl text-xl">
                 <h1 className='text-white lg:leading-65 leading-10'>
                     <span className='highlight'>My name is <span className="no-drop">Ethan Mathieu.</span></span>
