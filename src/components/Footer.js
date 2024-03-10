b0be7c90c5653d9bd30c2f66c80149cc940f1b14
Ethan Mathieu <95326225+emath12@users.noreply.github.com>
2024-03-10 03:50:55 -0400

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,9 +15,7 @@ export default function Footer() {
         <MDBFooter className='lg:text-center' color='white' bgColor='transparent'>
         <MDBContainer className='p-4'>
  
-            <a target="_blank" href="https://www.youtube.com/watch?v=Ww5-UV_Dv_U">
-              <p className='opacity-50'><strong>"Follow your passion, and life will reward you." <br/> -Uncle Iroh, <italic>Avatar The Last Airbender</italic></strong></p>
-            </a>
+    
 
         </MDBContainer>
         </MDBFooter>
